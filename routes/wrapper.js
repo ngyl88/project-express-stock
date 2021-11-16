88c330e49d3ea2472544204077668545fe11cca9
ngyl88 <ng_yl88@hotmail.com>
2021-11-17 00:49:29 +0800

    add cors onto /wrapper routes

--- routes/wrapper.js
+++ routes/wrapper.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
+const cors = require('cors');
+const corsOptions = {
+  origin: process.env.REACT_STOCK_APP_FRONTEND_URL,
+  // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 const externalService = require("../middleware/externalService");
 
 const router = express.Router();
+router.use(cors())
 router.use(express.json());
 
-router.get("/", externalService.queryAlphaVantage);
+router.get("/", cors(corsOptions), externalService.queryAlphaVantage);
 
 module.exports = app => {
   app.use("/wrapper", externalService.queryAlphaVantage);
