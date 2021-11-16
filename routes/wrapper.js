d59e0a018af742bfd39e20d65947aa721ddcd06b
ngyl88 <ng_yl88@hotmail.com>
2021-11-17 02:02:27 +0800

    modify corsOptions

--- routes/wrapper.js
+++ routes/wrapper.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const cors = require('cors');
 const corsOptions = {
-  origin: [process.env.REACT_STOCK_APP_FRONTEND_URL],
+  origin: process.env.REACT_STOCK_APP_FRONTEND_URL || '*',
   methods: "GET,HEAD,OPTIONS",
   preflightContinue: true,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
