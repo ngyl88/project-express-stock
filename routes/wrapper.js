d8ec1a7e4c6646d1110737497259d3a530f2a1b2
ngyl88 <ng_yl88@hotmail.com>
2021-11-17 00:58:52 +0800

    configure corsOptions

--- routes/wrapper.js
+++ routes/wrapper.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const cors = require('cors');
 const corsOptions = {
-  origin: process.env.REACT_STOCK_APP_FRONTEND_URL,
-  // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: [process.env.REACT_STOCK_APP_FRONTEND_URL],
+  preflightContinue: true,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
 const externalService = require("../middleware/externalService");
